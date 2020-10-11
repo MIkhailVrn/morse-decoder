8c081cc388d804ff51f38061c267e9f3384806f7
Mik <mihail.bindyukov@gmail.com>
2020-10-12 02:33:49 +0300

    feat: all test are ok

--- src/index.js
+++ src/index.js
@@ -39,6 +39,33 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let result = '';
+    let expres = expr
+    while (expres.length > 0) {
+        let code = '';
+        if (expres.substr(0,10) === '**********'){
+            result += ' ';
+            expres = expres.substr(10, expres.length - 1);
+            continue;
+        }
+        let part = (+expres.substr(0,10)).toString();
+        while (part.length > 0) {
+            let substrCurrent = part.substr(0,2);
+            switch (substrCurrent) {
+                case "10" :
+                    code += '.';
+                    break;
+                case "11":
+                    code += '-'
+                    break;
+            }
+            part = part.substr(2, part.length - 1);
+        }
+        result += MORSE_TABLE[code];
+        code = '';
+        expres = expres.substr(10, expres.length - 1)
+    }   
+    return result;
 }
 
 module.exports = {
